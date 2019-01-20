d4782cfe51b6ecd70d9c719b5d0f1b4b37851d61
amitnos123 <sami.nosatzki@gmail.com>
2019-01-20 15:33:06 +0200

    added HP

---
+++ gameProject/Assets/Libraries/characterParams.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterParams : MonoBehaviour
+{
+    public int health = 100;
+    public int maxHealth = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
